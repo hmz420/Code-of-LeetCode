c95a2005e4f02552118d8f3ede878c7f30546af3
ZHM420 <30405153+ZHM420@users.noreply.github.com>
2019-04-26 17:07:40 +0800

    Add one cpp file
    None

---
+++ Two Sum.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+	vector<int> twoSum(vector<int>& nums, int target)
+	{
+		vector<int> v;
+		unordered_map<int, int> hash;
+		for (int i = nums.size() - 1; i >= 0; hash[nums[i]] = i, i--)
+		{
+			if (hash.find(target - nums[i]) == hash.end()) continue;
+			v.push_back(i);
+			v.push_back(hash[target - nums[i]]);
+		}
+		return v;
+	}
+};
