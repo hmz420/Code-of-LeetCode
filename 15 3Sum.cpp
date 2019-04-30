fd7db2cd51bcdb2d4c3ab3d2ef10cb5d6c2496ea
ZHM420 <30405153+ZHM420@users.noreply.github.com>
2019-04-30 10:23:01 +0800

    Added two file.

---
+++ 15 3Sum.cpp
@@ -0,0 +1,55 @@
+﻿/*
+Method:
+  1）三指针法：三指针或者双指针有时候能够将时间复杂度降低一维；第一个指针用来定下第一个元素，第二三个指针用来搜索；
+  第二三个指针位于剩余元素的两端，通过两者之和与目标值的大小关系从两端不断逼近正确结果；
+  2)哈希表法：将第一个元素固定，那么就变成了两个数求和；根据Two Sum得知，两数之和有几种方法，可以调用；
+
+Note:
+  1）sort可以进行vector的元素大小排序；
+  2）双指针法，对于有序数组来说，通过逼近的方法可以降低时间复杂度；
+*/
+
+class Solution {
+public:
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		vector<vector<int>> res;
+		int l = nums.size();
+
+		if (l <= 2) return res;
+		else
+		{
+			for (int i = 0; i < l - 2; i++)
+			{
+				if (nums[i] > 0) return res;
+				if (i == 0 || nums[i] != nums[i - 1])
+				{
+					int left = i + 1, right = l - 1;
+					int target = -nums[i];
+
+					while (left < right)
+					{
+						if (nums[left] + nums[right] == target)
+						{
+							res.push_back(vector<int>{nums[i], nums[left], nums[right]});
+
+							while (left < right && nums[left + 1] == nums[left]) 
+								++left;
+							while (left < right && nums[right - 1] == nums[right]) 
+								--right;
+							
+							++left;
+							--right;
+						}
+						else if (nums[left] + nums[right] <= target) 
+							++left;
+						else 
+							--right;
+					}
+				}
+			}
+			return res;
+		}
+	}
+};
+
---
+++ 16 3Sum Closest.cpp
@@ -0,0 +1,41 @@
+﻿/*
+Method:
+  解法：15 3Sum 中已经探讨了三个数之和的较为有效率的算法，此题类似，采用三指针的方式；
+  区别就是：此题的后两个指针在搜寻过程终止的条件发生了改变，通过与目标值之间的差值，对结果进行迭代；
+
+Note:
+  避免固化思维：定下一个指针，后两个指针的和与目标差值进行比较，这样会多出一步计算过程，即计算目标值与
+  第一个指针数值之间的差值；此类情况有时方便，有时复杂，视情况而定；
+*/
+
+class Solution {
+public:
+	int threeSumClosest(vector<int>& nums, int target) {
+		sort(nums.begin(), nums.end());
+		int res = INT_MAX, len = nums.size();
+		int left, right = 0;
+		int nums0, sum, min_z = INT_MAX;
+
+		for (int i = 0; i < len - 2; ++i)
+		{
+			nums0 = nums[i];
+			for (left = i + 1, right = len - 1; left < right;)
+			{
+				sum = nums0 + nums[left] + nums[right];
+				if (target == sum)
+					return sum;
+				if (abs(target - sum) < min_z)
+				{
+					res = sum;
+					min_z = abs(target - sum);
+				}
+				if (sum > target)
+					--right;
+				else
+					++left;
+			}
+		}
+		return res;
+	}
+};
+
