aad579e25043d9ab82454f88db68f6ffc56cb792
ZHM420 <30405153+ZHM420@users.noreply.github.com>
2019-05-05 22:15:32 +0800

    Changed

--- 18 4Sum.cpp
+++ 18 4Sum.cpp
@@ -14,33 +14,33 @@ public:
 		vector<vector<int>> res;
 		int len = nums.size();
 
-		for (int i = 0, sum = 0; i < len - 3; ++i) {
-			if (i != 0 && nums[i] == nums[i - 1])
-				continue;
-			if (nums[i] + nums[len - 3] + nums[len - 2] + nums[len - 1] < target)
-				continue;
-			if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target)
-				break;
-			for (int j = i + 1; j < len - 2; j++) {
-				if (j != i + 1 && nums[j] == nums[j - 1])
-					continue;
-				if (nums[i] + nums[j] + nums[len - 2] + nums[len - 1] < target)
-					continue;
-				if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target)
-					break;
+		for (int i = 0, sum = 0; i < len - 3; ++i)
+		{
+			if (i != 0 && nums[i] == nums[i - 1]) continue;
+			if (nums[i] + nums[len - 3] + nums[len - 2] + nums[len - 1] < target) continue;
+			if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+			
+			for (int j = i + 1; j < len - 2; j++) 
+			{
+				if (j != i + 1 && nums[j] == nums[j - 1]) continue;
+				if (nums[i] + nums[j] + nums[len - 2] + nums[len - 1] < target) continue;
+				if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
 				int left = j + 1, right = len - 1;
-				while (left < right) {
+				
+				while (left < right) 
+				{
 					sum = nums[i] + nums[j] + nums[left] + nums[right];
-					if (sum < target)
-						++left;
-					else if (sum > target)
-						--right;
-					else {
+					if (sum < target) ++left;
+					else if (sum > target) --right;
+					else 
+					{
 						res.push_back(vector<int> {nums[i], nums[j], nums[left], nums[right]});
+
 						while (left < right && nums[left + 1] == nums[left])
 							++left;
 						while (left < right && nums[right - 1] == nums[right])
 							--right;
+
 						++left;
 						--right;
 					}
